18da6bc627134f28944b5b4064d4d9ffe8cadb04
mini-777 <pho0570@gmail.com>
2023-08-09 17:03:15 +0900

    Add Start.tsx

--- src/Start.tsx
+++ src/Start.tsx
@@ -1,81 +1,48 @@
 import * as React from 'react';
-import { Button, Box, Center, Image, Text, VStack, HStack } from 'native-base';
-
-import { Animated } from 'react-native';
-import axios from 'axios';
+import {
+  Button,
+  Box,
+  Center,
+  Image,
+  Text,
+  VStack,
+  HStack,
+  Heading,
+} from 'native-base';
+import Layout from './Layout';
 const Start = ({ navigation, route }: any) => {
-  const ComponentA = () => (
-    <Center>
-      <HStack>
-        <Text bold fontSize='30' color='#675DE7'>
-          AI
-        </Text>
-        <Text fontSize={'30'}>가</Text>
-      </HStack>
-      <HStack>
-        <Text bold fontSize={'30'} color='#675DE7'>
-          대화
-        </Text>
-        <Text fontSize={'30'}>를 정리하고 있어요</Text>
-      </HStack>
-    </Center>
-  );
-  const ComponentB = () => (
-    <Box>
-      <HStack>
-        <Text bold fontSize='30' color='#675DE7'>
-          여행계획
-        </Text>
-        <Text fontSize='30'>을 정리하고 있어요</Text>
-      </HStack>
-    </Box>
-  );
-  const ComponentC = () => (
-    <Center>
-      <HStack>
-        <Text bold fontSize='30' color='#675DE7'>
-          생각
-        </Text>
-        <Text fontSize={30}>이 많아질땐 </Text>
-      </HStack>
-      <HStack>
-        <Text bold fontSize='30' color='#675DE7'>
-          여행
-        </Text>
-        <Text fontSize={30}>을 떠나보세요</Text>
-      </HStack>
-    </Center>
-  );
-  const ComponentD = () => (
-    <Text bold fontSize='30' color='#675DE7'>
-      거의 끝났어요
-    </Text>
-  );
-
-  const components = [ComponentA, ComponentB, ComponentC, ComponentD];
-
-  const ComponentToRender = components[index];
-
   return (
-    <Center>
+    <Box>
+      <Layout navigation={navigation} />
       <Box mt='40'>
-        <Animated.View style={{ opacity: fadeAnim }}>
-          <ComponentToRender />
-        </Animated.View>
+        <Heading>환영합니다</Heading>
       </Box>
       <Image
-        source={require('../assets/image/Loading.png')}
+        source={require('../assets/image/Second.png')}
         alt='loading image'
-        size={400}
+        w={10}
+        h={2}
         style={{
           position: 'absolute',
-          top: 250,
-          left: 0,
-          right: 0,
-          bottom: 0,
+          top: 100,
+          right: 10,
         }}
       ></Image>
-    </Center>
+      <Center>
+        <Box position={'absolute'} top={550}>
+          <Button
+            h={60}
+            onPress={() => navigation.navigate('Where')}
+            w='350'
+            rounded='2xl'
+            colorScheme='indigo'
+            _text={{ fontSize: 20, bold: true }}
+          >
+            이 그림으로 저장할래요
+          </Button>
+        </Box>
+      </Center>
+    </Box>
   );
 };
 
